ae16e3c6db9f38fdba994da0f6ff633fb01e8565
Alexander <al.x.falconer@gmail.com>
2020-12-08 16:16:33 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
 def countdown
+countdown = 10
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        return "HAPPY NEW YEAR!"
+    end
 end
+
